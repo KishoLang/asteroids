85e0818340f1799fed50f502024658eab4093e59
KishoLang <aklangenberg@gmail.com>
2024-11-07 23:06:32 +0100

    add white triangle on refreshing black screen

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    time = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        tick_val = time.tick(60)
+        dt = tick_val / 1000
+        
 
 if __name__ == "__main__":
     main()
